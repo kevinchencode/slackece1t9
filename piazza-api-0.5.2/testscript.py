b174565b0f2e09c10c77421d14b5507b4ad6c7a0
michaelvu97 <michael.vu.97@outlook.com>
2016-09-21 17:27:43 -0400

    piazza post retrieval

--- piazza-api-0.5.2/testscript.py
+++ piazza-api-0.5.2/testscript.py
@@ -1,20 +1,66 @@
+"""
+#
+# Testscript by Mike Vu.
+# Used to adapt the piazza api.
+# Created: 20/09/2016.
+#
+"""
+
 from piazza_api import Piazza
+import getpass
 
 p = Piazza()
+"""
+#
+# email = input('Enter your email: ')
+# password = getpass.getpass('Enter your password: ')
+#
+"""
+email = "mike.vu@mail.utoronto.ca"
+password = "i1puvehtoD"[::-1]
 
-email = input('Enter your email: ')
-password = input('Enter your password: ')
 p.user_login(email, password)
 
+"""
+#
+# Get user profile and classes.
+#
+"""
 user_profile = p.get_user_profile()
 classes = p.get_user_classes()
 for i in classes:
     print(i['num'])
 
-selected_class = input('Choose class: ')
-selected_class = selected_class.lower()
+"""
+#
+# Class selector.
+#
+"""
+selected_class_string = input('Choose class: ')
+selected_class_string = selected_class_string.lower()
+selected_class = None
 for i in classes:
-    if i['num'].lower() == selected_class:
-        print ('Course selected, ID = ')
-        print (str(i['nid']))
-        
+    if i['num'].lower() == selected_class_string:
+        selected_class = i
+    
+print(i)
+
+"""
+#
+# Get network data for the class.
+#
+"""
+ece241 = p.network(i['nid'])
+#ece241.get_post(1)
+posts = ece241.iter_all_posts(1)
+for post in posts:
+    print(post)
+    
+## TODO: CREATE A OBJECT/CLASS FOR POST
+
+"""
+#
+# Structure for post objects:
+# see '/post_response_format.txt'
+#
+"""
