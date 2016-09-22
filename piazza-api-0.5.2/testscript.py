906991d49abed475d0ca44b582be9aeb54b94d08
michaelvu97 <michael.vu.97@outlook.com>
2016-09-22 09:27:34 -0400

    Fixed class selector
    Class selector wasn't properly stopping at the correct class. Also added
    a method for dumping input to a file (for debugging).

--- piazza-api-0.5.2/testscript.py
+++ piazza-api-0.5.2/testscript.py
@@ -6,6 +6,19 @@
 #
 """
 
+
+def dump_response_to_file (post='', append=False):
+    """
+    #
+    # For debugging: writes the reponse to a file.
+    # TODO: check that every time this is called, it doesn't clear the file, and
+    #       rather appends to the file.
+    #
+    """
+    f = open('output.txt', 'a' if append else 'w')
+    f.write(repr(post))
+
+
 from piazza_api import Piazza
 import getpass
 
@@ -34,14 +47,17 @@ for i in classes:
 """
 #
 # Class selector.
+# TODO: catch invalid input.
 #
 """
 selected_class_string = input('Choose class: ')
 selected_class_string = selected_class_string.lower()
 selected_class = None
 for i in classes:
+    print ("{}: {}".format(i['num'],i['num'].lower() == selected_class_string))
     if i['num'].lower() == selected_class_string:
         selected_class = i
+        break # because for some reason, selected class is changed every time.
     
 print(i)
 
@@ -54,7 +70,7 @@ ece241 = p.network(i['nid'])
 #ece241.get_post(1)
 posts = ece241.iter_all_posts(1)
 for post in posts:
-    print(post)
+    dump_response_to_file(post, True)
     
 ## TODO: CREATE A OBJECT/CLASS FOR POST
 
