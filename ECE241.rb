07a89697d21f9a1255a746b7699b3d119835d317
Kevin <kevinchencode@gmail.com>
2016-09-22 16:45:08 -0400

    made slackbot more modular

--- ECE241.rb
+++ ECE241.rb
@@ -1,3 +1,8 @@
-message = ARGV[0]
-hello = `curl -X POST --data-urlencode 'payload={"channel": "#developers", "username": "digitalsystemsbot", "text": "#{ARGV[0]}", "icon_url": "https://avatars.slack-edge.com/2016-09-20/81985374709_3d122dfc770e342ead21_48.jpg"}' https://hooks.slack.com/services/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`
-puts hello
+module Slackbots
+  def self.speak(payload, hook_url)    
+    script_success = `curl -X POST --data-urlencode 'payload=#{payload}' #{hook_url}`
+    puts script_success
+  end
+  def 
+end
+
